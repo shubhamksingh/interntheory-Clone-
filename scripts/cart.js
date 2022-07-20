d4d859300abf20aca96a87ab728e4f0ed92d60a7
shubham <shubh0795@gmail.com>
2022-07-21 04:17:48 +0530

    day 2 issues

---
+++ scripts/cart.js
@@ -0,0 +1,12 @@
+import { navbar, footer, sidebar } from "../components/components.js";
+
+const x = document.getElementById('navbarMenu');
+x.innerHTML = navbar();
+
+const f = document.getElementById('footMianBox');
+f.innerHTML = footer();
+
+
+
+
+
